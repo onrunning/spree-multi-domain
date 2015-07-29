35126d0f12c345967df9c66de85053b7e0d40235
Emile <emile.swarts123@gmail.com>
2015-07-29 16:22:33 +0100

    Implement fix for products being unassociated from stores, and not redirect when accessing pricebooks

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -3,7 +3,18 @@ Spree::Admin::ProductsController.class_eval do
 
   private
   def set_stores
-    @product.store_ids = nil unless params[:product].key? :store_ids
+    if user_comes_from_product_details? && no_stores_selected?
+      @product.store_ids = nil
+    end
+  end
+
+  def user_comes_from_product_details?
+    params = Spree::Core::Engine.routes.recognize_path(request.referer)
+    params[:controller] == "spree/admin/products" && params[:action] == "edit"
+  end
+
+  def no_stores_selected?
+    params[:product][:store_ids].blank?
   end
 
 end
