56650c861d275025fa5aa2ecd79cfebd402352fd
Emile <emile.swarts123@gmail.com>
2015-07-30 11:47:47 +0100

    Once again update this to work in conjunction with spree price books, and spree I18n. For some reason not setting this before update filter will 404 when trying to access the translations of a product. This whole setup needs to be looked at again, but the bugs are solved for now.

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,20 +1,7 @@
 Spree::Admin::ProductsController.class_eval do
-  update.before :set_stores
+  before_filter :set_stores, only: [:update]
 
   private
   def set_stores
-    if user_comes_from_product_details? && no_stores_selected?
-      @product.store_ids = nil
-    end
   end
-
-  def user_comes_from_product_details?
-    params = Spree::Core::Engine.routes.recognize_path(request.referer)
-    params[:controller] == "spree/admin/products" && params[:action] == "edit"
-  end
-
-  def no_stores_selected?
-    params[:product][:store_ids].blank?
-  end
-
 end
